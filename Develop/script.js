931c4c5b3bd295570719d5460f1c33127fbc683b
JulianF <floydjulian1991@gmail.com>
2022-03-05 15:03:39 -0800

    Managed to get the current date to display on the top of the page

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,12 @@
 // Acquire whatever html element you want with jquery or javascript and save them as global variables here
+var todayDate = document.getElementById("currentDay"); 
 
 // Create a function to display current date on the top of the page, using either jquery and/or moment.js
-
+ var currentDate = new Date();
+ var cDay = currentDate.getDate();
+ var cMonth = currentDate.getMonth() + 1;
+ var cYear = currentDate.getFullYear();
+ todayDate.innerHTML = " The Current Date is " + " : " + cMonth + "/" + cDay + "/" + cYear;
 // Create a function to turn event block into text area
 
 // Create a function to turn text area back to a div or p element
