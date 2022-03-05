c1626896b6d840de9b5cb247acfc0a0bc0272bfa
JulianF <floydjulian1991@gmail.com>
2022-03-05 15:03:04 -0800

    Added pseudo code, tenative outline of js code, rounded the save block with bootstrap, will continue to round corners

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+// Acquire whatever html element you want with jquery or javascript and save them as global variables here
+
+// Create a function to display current date on the top of the page, using either jquery and/or moment.js
+
+// Create a function to turn event block into text area
+
+// Create a function to turn text area back to a div or p element
+
+// Create a function to save event into localStorage when eventlistener click for save block.
+
+// Not sure if this will be in js file, code to color code past hour/events, current hour/events, and future hours/events 
